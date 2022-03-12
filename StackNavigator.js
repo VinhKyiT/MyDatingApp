ab7e9832fd3d7b4b139b9b23227e912c33bd18bf
vinhkyit <vinhkyit2905@gmail.com>
2022-03-12 15:44:54 +0700

    init login and chat screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
+import ChatScreen from './src/screens/ChatScreen/ChatScreen';
+import LoginScreen from './src/screens/LoginScreen/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +11,8 @@ const StackNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 };
