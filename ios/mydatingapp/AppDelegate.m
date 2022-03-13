7f17a00752aec3591faf615367a0515b34c3d7b5
VinhKyIT <vinhkyit2905@gmail.com>
2022-03-13 11:49:01 +0700

    google authentication

--- ios/mydatingapp/AppDelegate.m
+++ ios/mydatingapp/AppDelegate.m
@@ -59,4 +59,11 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 }
 
+// - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *, id> *) options {
+//   if ([self.authorizationFlowManagerDelegate resumeExternalUserAgentFlowWithURL:url]) {
+//     return YES;
+//   }
+//   return [RCTLinkingManager application:app openURL:url options:options];
+//  }
+
 @end
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,41 +1,42 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { authorize } from 'react-native-app-auth';
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [error, setError] = useState();
   const signInWithGoogle = async () => {
-    // const config = {
-    //   issuer: 'https://accounts.google.com',
-    //   iosClientId:
-    //     '696534455324-7fn16cjgbd1kdb7s3t097foj3jttb2s0.apps.googleusercontent.com',
-    //   clientId:
-    //     '696534455324-6s4tgsvsrsgama298bph1g3ltmegkca6.apps.googleusercontent.com',
-    //   scopes: ['profile', 'email'],
-    //   redirectUrl:
-    //     'com.googleusercontent.apps.696534455324-6s4tgsvsrsgama298bph1g3ltmegkca6:/oauth2redirect',
-    //   permissions: ['public_profile', 'email', 'gender', 'location'],
-    // };
-
-    const config = {
+    const iosConfig = {
       issuer: 'https://accounts.google.com',
       clientId:
         '696534455324-7fn16cjgbd1kdb7s3t097foj3jttb2s0.apps.googleusercontent.com',
       redirectUrl:
         'com.googleusercontent.apps.696534455324-7fn16cjgbd1kdb7s3t097foj3jttb2s0:/oauth2redirect/google',
-      scopes: ['openid', 'profile'],
+      scopes: ['openid', 'profile', 'email'],
+    };
+
+    const androidConfig = {
+      issuer: 'https://accounts.google.com',
+      clientId:
+        '696534455324-e099g2i1mhgq9thmh8f3m0lcsksn52h1.apps.googleusercontent.com',
+      redirectUrl:
+        'com.googleusercontent.apps.696534455324-e099g2i1mhgq9thmh8f3m0lcsksn52h1:/oauth2redirect/google',
+      scopes: ['openid', 'profile', 'email'],
     };
 
     try {
-      const result = await authorize(config);
-      console.log(result);
+      const result = await authorize(iosConfig);
+      if (result) setUser(result);
+      console.log({ result });
     } catch (error) {
-      console.log(error);
+      setError(error);
+      console.log({ error });
     }
   };
 
   return (
-    <AuthContext.Provider value={{ user: null, signInWithGoogle }}>
+    <AuthContext.Provider value={{ user: user, error, signInWithGoogle }}>
       {children}
     </AuthContext.Provider>
   );
