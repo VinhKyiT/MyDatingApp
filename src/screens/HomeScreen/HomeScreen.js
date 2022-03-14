4935c022a7588fab6835a5a1d4eca19856dfe651
vinhkyit <vinhkyit2905@gmail.com>
2022-03-14 17:49:16 +0700

    swipe function

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -5,25 +5,41 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Image,
+  StyleSheet,
+  Entypo,
 } from 'react-native';
-import React from 'react';
+import React, { useRef } from 'react';
 import { useTailwind } from 'tailwind-rn/dist';
 import { useNavigation } from '@react-navigation/native';
 import useAuth from '../../hooks/useAuth';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Swiper from 'react-native-deck-swiper';
 
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+});
+
 const HomeScreen = () => {
   const navigation = useNavigation();
   const tw = useTailwind();
   const { user, logOut } = useAuth();
+  const swipeRef = useRef(null);
 
   const DEMO_DATA = [
     {
       id: 1,
       firstName: 'Ky',
       lastName: 'Bui',
-      occupation: 'Software Engineer',
+      job: 'Software Engineer',
       photoURL: 'https://avatars.githubusercontent.com/u/38911691?v=4',
       age: '21',
     },
@@ -31,7 +47,7 @@ const HomeScreen = () => {
       id: 2,
       firstName: 'Anh',
       lastName: 'Nguyen',
-      occupation: 'Software Engineer',
+      job: 'Software Engineer',
       photoURL: 'https://avatars.githubusercontent.com/u/86947758?v=4',
       age: '22',
     },
@@ -39,7 +55,7 @@ const HomeScreen = () => {
       id: 3,
       firstName: 'Trieu',
       lastName: 'Tran',
-      occupation: 'Software Engineer',
+      job: 'Software Engineer',
       photoURL: 'https://avatars.githubusercontent.com/u/79587394?v=4',
       age: '23',
     },
@@ -47,7 +63,7 @@ const HomeScreen = () => {
       id: 4,
       firstName: 'An',
       lastName: 'Nguyen',
-      occupation: 'Software Engineer',
+      job: 'Software Engineer',
       photoURL: 'https://avatars.githubusercontent.com/u/79587394?v=4',
       age: '24',
     },
@@ -55,7 +71,7 @@ const HomeScreen = () => {
       id: 5,
       firstName: 'Tin',
       lastName: 'Mai',
-      occupation: 'Software Engineer',
+      job: 'Software Engineer',
       photoURL: 'https://avatars.githubusercontent.com/u/73238680?v=4',
       age: '25',
     },
@@ -87,18 +103,81 @@ const HomeScreen = () => {
 
       <View style={tw('flex-1 -mt-6')}>
         <Swiper
+          ref={swipeRef}
           containerStyle={{ backgroundColor: 'transparent' }}
-          stackSize={5}
+          stackSize={3}
+          cardIndex={0}
+          animateCardOpacity
+          verticalSwipe={false}
+          overlayLabels={{
+            left: {
+              title: 'NOPE',
+              style: {
+                label: {
+                  textAlign: 'right',
+                  color: 'red',
+                },
+              },
+            },
+            right: {
+              title: 'LIKE',
+              style: {
+                label: {
+                  color: 'green',
+                },
+              },
+            },
+          }}
+          onSwipedLeft={() => {
+            console.log('swipe nope');
+          }}
+          onSwipedRight={() => {
+            console.log('swipe like');
+          }}
+          backgroundColor={'#4FD0E9'}
           cards={DEMO_DATA}
           renderCard={card => (
-            <View key={card.id} style={tw('bg-white h-3/4 rounded-xl')}>
+            <View
+              key={card.id}
+              style={tw('relative bg-white h-3/4 rounded-xl')}>
               <Image
-                style={tw('w-full h-full rounded-xl')}
+                style={tw('absolute top-0 w-full h-full rounded-xl')}
                 source={{ uri: card.photoURL }}
               />
+              <View
+                style={[
+                  tw(
+                    'absolute bottom-0 flex-row justify-between items-center bg-white w-full h-20 px-6 rounded-b-xl',
+                  ),
+                  styles.cardShadow,
+                ]}>
+                <View>
+                  <Text style={tw('text-xl font-bold')}>
+                    {card.firstName} {card.lastName}
+                  </Text>
+                  <Text>{card.job}</Text>
+                </View>
+                <Text style={tw('text-2xl font-bold')}>{card.age}</Text>
+              </View>
             </View>
           )}
         />
+      </View>
+      <View style={tw('flex flex-row justify-evenly mb-5')}>
+        <TouchableOpacity
+          onPress={() => swipeRef.current.swipeLeft()}
+          style={tw(
+            'items-center justify-center rounded-full w-16 h-16 bg-red-200',
+          )}>
+          <Icon name="close-sharp" size={24} color="red" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => swipeRef.current.swipeRight()}
+          style={tw(
+            'items-center justify-center rounded-full w-16 h-16 bg-green-200',
+          )}>
+          <Icon name="heart" size={24} color="green" />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
